e4748dc856e31a1bcc889dac7ffa6980adb76183
Yauhen Yakimovich <yy@symmetrics.de>
2010-05-26 19:57:53 +0200

    Die Klasse Symmetrics_TweaksGerman_Block_TaxRender wurde in Symmetrics_TweaksGerman_Block_Tax_Render umbenannt

--- build/app/code/community/Symmetrics/TweaksGerman/Block/Tax/Render.php
+++ build/app/code/community/Symmetrics/TweaksGerman/Block/Tax/Render.php
@@ -33,7 +33,7 @@
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @link      http://www.symmetrics.de/
  */
-class Symmetrics_TweaksGerman_Block_TaxRender extends Mage_Checkout_Block_Total_Tax
+class Symmetrics_TweaksGerman_Block_Tax_Render extends Mage_Checkout_Block_Total_Tax
 {
     /**
      * Setter for totals

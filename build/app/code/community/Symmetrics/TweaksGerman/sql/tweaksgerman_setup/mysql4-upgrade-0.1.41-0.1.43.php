e66f7fe2a70fc78868251e0358be84456ab14357
Benjamin Klein <bk@symmetrics.de>
2010-09-07 09:59:33 +0200

    MARKET-22 Add me as author clean up code: - delete wrong indents

--- build/app/code/community/Symmetrics/TweaksGerman/sql/tweaksgerman_setup/mysql4-upgrade-0.1.41-0.1.43.php
+++ build/app/code/community/Symmetrics/TweaksGerman/sql/tweaksgerman_setup/mysql4-upgrade-0.1.41-0.1.43.php
@@ -15,7 +15,7 @@
  * @category  Symmetrics
  * @package   Symmetrics_TweaksGerman
  * @author    symmetrics gmbh <info@symmetrics.de>
- * @author    Yauhen Yakimovich <yy@symmetrics.de>
+ * @author    Benjamin Klein <bk@symmetrics.de>
  * @copyright 2010 symmetrics gmbh
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @link      http://www.symmetrics.de/
@@ -29,27 +29,20 @@ $connection  = $this->getConnection();
 $regionTable = $installer->getTable('directory/country_region');
 
 $regionsToIns = array(
-    array('DE', 'DE', 'Nicht ausgewählt'),
-
+    array(
+        'DE',
+        'DE',
+        'Nicht ausgewählt'
+        )
 );
 
 foreach ($regionsToIns as $row) {
-    if (! ($connection->fetchOne(
-        "SELECT 1 FROM `{$regionTable}` WHERE `country_id` = :country_id && `code` = :code",
+    $connection->insert(
+        $regionTable,
         array(
-            'country_id' => $row[0],
-            'code' => $row[1]
-            )
-    )
-    )
-) {
-        $connection->insert(
-            $regionTable,
-            array(
-                'country_id'   => $row[0],
-                'code'         => $row[1],
-                'default_name' => $row[2]
-            )
-        );
-    }
+            'country_id'   => $row[0],
+            'code'         => $row[1],
+            'default_name' => $row[2]
+        )
+    );
 }

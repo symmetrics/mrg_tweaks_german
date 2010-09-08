cca04824acf98d7a60db8310d39f191d1fa115d4
Benjamin Klein <bk@symmetrics.de>
2010-09-08 18:09:51 +0200

    MARKET-22 Refactoring of "province.js". - Clening the Code -- Build from the old code a class -- same function --- Add 3 Observer for address edeting, shipping, billing
    Cleaning code of address.js
    - delete all functions and code snippets which controlled on of the 3 observer

--- build/js/symmetrics/tweaksgerman/province.js
+++ build/js/symmetrics/tweaksgerman/province.js
@@ -19,93 +19,236 @@
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @link      http://www.symmetrics.de/
  */
-document.observe('dom:loaded', function() {
-    /**
-     * Get country element from Dom.
-     */
-    var country = $('billing:country_id');
-    /**
-     * Setup billingFlag to register later if we are in billing.
-     */
-    var billingFlag = true;
-    /**
-     * Check if we have a object or not.
-     */
-    if (!country) {
-        /**
-         * Set billingFlag to False.
-         */
-        var billingFlag = false;
-        /**
-         * Set new object.
-         */
-        country = $('country');
-    }
-    /**
-     * Build observer for changes in country.
-     */
-    Event.observe(country,'change',function(){
-        setRegion(billingFlag);
-    });
-    /**
-     * Start setRegion.
-     * Musst be run here to hide "State/Province" when Germany is standart.
-     */
-    setRegion(billingFlag);
-});
 
-// setRegion returns nothing.
-// We do not an Boolean parameter to figure out if we need
-// a prefix
-// This function is looking for country "DE".
-// If country is Germany, hide "State/Province" text.
-// To equal Form settings we set region_id to the last in regions.
-// The last element was created by us in a update file.
-function setRegion(billingFlag) {
+if (!window.Symmetrics) {
+    window.Symmetrics = {};
+}
+
+/**
+ * Symmetrics.Province
+ * Generate lorem ipsum text
+ *
+ * @category  Symmetrics
+ * @package   Symmetrics_TweaksGerman
+ * @author    symmetrics gmbh <info@symmetrics.de>
+ * @author    Benjamin Klein <bk@symmetrics.de>
+ * @copyright 2010 symmetrics gmbh
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      http://www.symmetrics.de/
+ */
+Symmetrics.Province = Class.create();
+Object.extend(Object.extend(Symmetrics.Province.prototype, Abstract.prototype),
+{
     /**
-     * Checking for billingFlag.
-     * Setup prefix for billing or not.
-     * Setup country_prefix.
-     * Setup region_prefix.
-     * Setup form.
+     * Constructor initialize observer for dom loaded
      */
-    if (billingFlag) {
-        var prefix = 'billing:';
-        var country_prefix = '_id';
-        var region_prefix = '_id';
-        var form = 'co-billing-form';
-    } else {
-        var prefix = '';
-        var country_prefix = '';
-        var region_prefix = '_id';
-        var form = 'form-validate';
-    }
-    if ($(prefix + 'region' + region_prefix)) {
-        /**
-         * Check if country ist Germany.
-         */
-        if ($F(prefix + 'country' + country_prefix) == "DE") {
-            /**
-             * Hide Redion select Box.
-             */
-            $(prefix + 'region' + region_prefix).hide();
-            /**
-             * Hide text "State/Province".
-             */
-            $$('label[for="' + prefix + 'region' + region_prefix + '"]').first().hide();
-            /**
-             * Get last region from select box.
-             */
-            var region = $A($(prefix + 'region' + region_prefix).options).last();
-            /**
-             * Set RegionId from latest element.
-             */
-            $(prefix + 'region' + region_prefix).setValue(region.getAttribute('value'));
+    initialize: function()
+    {
+        this.createObserverDomLoaded();
+    },
+
+    /**
+     * createObserverDomLoaded
+     *
+     * @return void
+     */
+    createObserverDomLoaded: function()
+    {
+        document.observe('dom:loaded', (function()
+        {
+            var country = $('billing:country_id');
+            if (!country) {
+                console.log("choose address");
+                this.createObserverProvinceAddress();
+                this.startProvinceAdressChanging();
+            } else {
+                this.createObserverProvinceBilling();
+                this.createObserverProvinceShipping();
+                this.startProvinceBillingChanging();
+                this.startProvinceShippingChanging();
+            }
+        }).bind(this));
+    },
+
+    /**
+     * createObserverProvinceBilling
+     *
+     * @return void
+     */
+    createObserverProvinceBilling: function()
+    {
+        Event.observe($('billing:country_id'),'change', (function(){
+            this.startProvinceBillingChanging();
+        }).bind(this));
+    },
+
+    /**
+     * startProvinceBillingChanging
+     *
+     * @return void
+     */
+    startProvinceBillingChanging: function()
+    {
+        var countryFieldName = "billing:country_id";
+        var regionInputFieldName = "billing[region_id]";
+        var regionFieldName= 'billing:region_id';
+        this.setRegionId(countryFieldName, regionInputFieldName, regionFieldName);
+
+    },
+
+    /**
+     * createObserverProvinceShipping
+     *
+     * @return void
+     */
+    createObserverProvinceShipping: function ()
+    {
+        Event.observe($('shipping:country_id'),'change', (function(){
+            this.startProvinceShippingChanging();
+        }).bind(this));
+    },
+
+    /**
+     * startProvinceShippingChanging
+     *
+     * @return void
+     */
+    startProvinceShippingChanging: function()
+    {
+        var countryFieldName = "shipping:country_id";
+        var regionInputFieldName = "shipping[region]";
+        var regionFieldName = 'shipping:region_id';
+        var regionFieldNameLabel = 'shipping:region';
+        this.setRegionId(countryFieldName, regionInputFieldName, regionFieldName, regionFieldNameLabel);
+    },
+
+    /**
+     * createObserverProvinceAddress
+     *
+     * @return void
+     */
+    createObserverProvinceAddress: function()
+    {
+        Event.observe($('country'), 'change', (function(){
+            this.startProvinceAdressChanging();
+        }).bind(this));
+    },
+
+    /**
+     * startProvinceAdressChanging
+     *
+     * @return void
+     */
+    startProvinceAdressChanging: function()
+    {
+        var countryFieldName = "country";
+        var regionInputFieldName = "region_id";
+        var regionFieldName= 'region_id';
+        this.setRegionId(countryFieldName, regionInputFieldName, regionFieldName);
+    },
+
+    /**
+     * setRegionId
+     *
+     * @param String countryFieldName
+     * @param String regionInputFieldName
+     * @param String regionFieldName
+     *
+     * @return void
+     */
+    setRegionId: function(countryFieldName, regionInputFieldName, regionFieldName, regionFieldNameLabel)
+    {
+        try {
+            console.log(this.checkCountryCode(countryFieldName));
+            if (this.checkCountryCode(countryFieldName)) {
+                if (regionFieldNameLabel === undefined) {
+                    this.hideTextElement(regionFieldName);
+                } else {
+                    this.hideTextElement(regionFieldNameLabel);
+                }
+                this.updateRegionIdField(regionFieldName, regionInputFieldName, this.getLastRegionId(regionFieldName));
+            } else {
+                this.showTextElement(regionFieldName);
+            }
+        } catch (exception) {
+            console.log(exception);
+        }
+    },
+
+    /**
+     * hideTextElement
+     *
+     * @param String textFieldName The name of the "State/Province" field name
+     *
+     * @return void
+     */
+    hideTextElement: function(textFieldName)
+    {
+        console.log(textFieldName);
+        $$('label[for="' + textFieldName + '"]').first().hide();
+    },
+
+    /**
+     * showTextElement
+     *
+     * @param String textFieldName The name of the "State/Province" field name
+     *
+     * @return void
+     */
+    showTextElement: function(textFieldName)
+    {
+        $$('label[for="' + textFieldName + '"]').first().show();
+    },
+
+    /**
+     * deleteRegionIdField
+     *
+     * @param String fieldName       The Name of the RegionId field
+     * @param String regionFieldName Name of the new input field
+     * @param Int    regionId        Id of new RegionId
+     *
+     * @return void
+     */
+    updateRegionIdField: function(fieldName, regionFieldName, regionId)
+    {
+        var inputString = '<input type="hidden" name="' + regionFieldName +'" value="' + regionId + '">';
+        console.log($(fieldName));
+        console.log(inputString);
+        //To hide this element is recomment couse Magento work with it on change Country
+        $(fieldName).hide();
+        $(fieldName).update(inputString);
+    },
+
+    /**
+     * getLastRegionId
+     *
+     * @param String fieldName The Name of the RegionId field
+     *
+     * @return int
+     */
+    getLastRegionId: function(fieldName)
+    {
+        var region = $A($(fieldName).options).last();
+        
+        return region.getAttribute('value');
+    },
+
+    /**
+     * checkCountryCode
+     *
+     * @param String countryFieldName The Name of country field
+     *
+     * @return boolean
+     */
+    checkCountryCode: function(countryFieldName)
+    {
+        if ($F(countryFieldName) == 'DE') {
+            return true
         } else {
-            /**
-             * Show the "State/Province" text.
-             */
-            $$('label[for="' + prefix + 'region' + region_prefix + '"]').first().show();
+            return false;
         }
     }
-}
+
+
+});
+new Symmetrics.Province();

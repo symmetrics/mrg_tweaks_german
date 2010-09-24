9cf3603bd652811ea824d150e73d7b343ebe0c1f
Andreas Timm <at@symmetrics.de>
2010-09-24 16:10:41 +0200

    CR-196 (MARKET-21): Fixed a code template info.phtml in adminhtml, now used method of Mage::getDesign()->getSkinUrl() to invoke the skin url.

--- build/js/symmetrics/tweaksgerman/mrg_banner.js
+++ build/js/symmetrics/tweaksgerman/mrg_banner.js
@@ -15,6 +15,7 @@
  * @package   Symmetrics_TweaksGerman
  * @author    symmetrics gmbh <info@symmetrics.de>
  * @author    Yauhen Yakimovich <yy@symmetrics.de>
+ * @author    Andreas Timm <at@symmetrics.de>
  * @copyright 2010 symmetrics gmbh
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @link      http://www.symmetrics.de/
@@ -24,16 +25,6 @@
  * MRG Banner hooks
  */
 document.observe('dom:loaded', function() {
-    var baseUrl = BLANK_IMG.sub('js/spacer.gif','');
-    var removeRelative = function(path){
-        while (path.startsWith('../')) {
-            path = path.sub('../','');
-        }
-        return path;
-    }
-    $$('img.mrgimg').each(function(img){
-       img.writeAttribute('src', baseUrl + removeRelative(img.readAttribute('src')));
-    });
     // Unfold MRG info Banner
     var banner = $('general_info-head');
     if (banner) {
